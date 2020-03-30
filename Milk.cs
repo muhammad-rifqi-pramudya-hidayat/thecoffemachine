fd427c1a9bc29d88e303401a85e1e486fa4b54c5
muhammad-rifqi-pramudya-hidayat <62230963+muhammad-rifqi-pramudya-hidayat@users.noreply.github.com>
2020-03-30 20:24:52 +0700

    Add files via upload

---
+++ Milk.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCoffeMachine
+{
+    class Milk
+    {
+        private int volume = 0;//mililiter
+        private int volumeOneCup = 100;
+
+        public Milk(int volume)
+        {
+            this.volume = volume;
+        }
+
+        public Boolean isAvailable()
+        {
+            return volume >= volumeOneCup;
+        }
+
+        public void milk(int volume)
+        {
+            this.volume += volume;
+        }
+
+        public int makeOneCup()
+        {
+            this.volume = this.volume - this.volumeOneCup;
+            return volumeOneCup;
+        }
+
+        public int getVolume()
+        {
+            return this.volume;
+        }
+    }
+}
---
+++ TheCoffeMachine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCoffeMachine
+{
+    class CoffeMechine
+    {
+        private CoffePowder coffePowder;
+        private WaterGalon waterGalon;
+        private Milk milk;
+
+        public CoffeMechine(CoffePowder powder, WaterGalon galon, Milk milk)
+        {
+            this.waterGalon = galon;
+            this.coffePowder = powder;
+            this.milk = milk;
+        }
+
+        public String makeEsspresso()
+        {
+            if (!this.waterGalon.isAvailable())
+            {
+                return "Sorry, the water is empty";
+            }
+            if (!this.coffePowder.isAvailable())
+            {
+                return "Sorry, the coffe is empty";
+            }
+
+            this.waterGalon.makeOneCup();
+            this.coffePowder.makeOneCup();
+            return "Yey! your coffe Esspresso is ready";
+        }
+
+        public String makeCappucino()
+        {
+            if (!this.waterGalon.isAvailable())
+            {
+                return "Sorry, the water is empty";
+            }
+            if (!this.coffePowder.isAvailable())
+            {
+                return "Sorry, the coffe is empty";
+            }
+            if (!this.milk.isAvailable())
+            {
+                return "Sorry, the milk is empty";
+            }
+
+            this.waterGalon.makeOneCup();
+            this.coffePowder.makeOneCup();
+            this.milk.makeOneCup();
+            return "Yey! your coffe Cappucino is ready";
+        }
+
+        public String checkAvailability()
+        {
+            return $"the water :{this.waterGalon.getVolume()}  the coffe powder : {this.coffePowder.getNetto()} and the milk :{this.milk.getVolume()}";
+        }
+    }
+}
